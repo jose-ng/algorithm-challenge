1dbd1b36566005031352b43eaf7bf9f43b41714e
@x138058 <josenuez@empresaexterna.com.mx>
2024-02-27 13:14:33 -0600

    add support for spanish characters

---
+++ challenges/alphanumeric-string-sort/index.es-support.test.js
@@ -0,0 +1,7 @@
+const op = require("./index.es-support");
+
+fdescribe("Alphanumeric String Sort (Spanish characters support", () => {
+  it("Test input 1", () => {
+    expect(op('10bY¨^3cáéXZ2!ñÑ\"Üü')).toBe('üñéácbÑÜXYZ3120¨^!\"');
+  });
+});
